151480b6776222eaf93d7ab6c5dc4bc0f18db3dd
Genetica HULPP <geneticalapaz@salud.madrid.org>
2025-07-02 15:29:05 +0200

    redid package structure

--- frag16S/generate_database.py
+++ frag16S/generate_database.py
@@ -5,55 +5,26 @@ from Bio.SeqRecord import SeqRecord
 from Bio.Seq import Seq
 from Bio import SeqIO
 from skbio import DNA
+from logger import (
+    setup_logging,
+    log_step,
+)
+from utils import (
+    extract_metadata,
+    build_primer_dict,
+    convert_ambiguous_to_regex,
+)
 import typing as tp
 import pandas as pd
 import joblib as jb
 import numpy as np
-import sys, os, re
 import itertools
-import functools
 import traceback
 import tempfile
-import builtins
-import hashlib
 import inspect
-import pickle
-import shutil
+import logging
+import os, re
 import json
-
-        
-
-def hash_sequence(sequence, size=10):
-    h = hashlib.blake2b(digest_size=size)
-    h.update(pickle.dumps(sequence))
-    return h.hexdigest()
-
-
-def extract_metadata(sequence: SeqRecord) -> tp.Dict[str, str]:
-    metadata = dict(
-        seqid=sequence.id,
-        seqname=sequence.name,
-        seqhash=hash_sequence(sequence.seq),
-        description=sequence.description,
-    )
-    metadata["uname"] = "sequence_{id}_{name}_{hash}".format(
-        id=metadata["seqid"],
-        name=metadata["seqname"],
-        hash=metadata["seqhash"],
-    )
-    search = re.search(
-        r"(\d+) ([\w.]+) ([\w. ]+) (\w+;.+;) ([\w]+)",
-        metadata.get("description", "")
-    )
-    for i, key in enumerate([
-        "dscid",
-        "strnid",
-        "strnname",
-        "taxon",
-        "otu",
-    ]):
-        metadata[key] = search.group(i + 1) if search else "undefined"
-    return metadata
 
 
 def reducer_worker(i, j, seq_i, seq_j):
@@ -165,14 +136,6 @@ def reduce_by_centroid(
             SeqIO.write(seq, f, "fasta")
     return temp_fasta_file
         
-
-def convert_ambiguous_to_regex(primer: str):
-    REGEXER = json.load(open("pattern.json", "r"))
-    regex = ""
-    for i in range(len(primer)):
-        regex += REGEXER[primer[i]]
-    return regex
-
 
 def exact_matcher(
     sequence: SeqRecord,
@@ -312,24 +275,6 @@ def find_fragment(
     return Seq(seq[match5:match3])
 
 
-def build_primer_dict(
-    primer_file: str,
-) -> tp.Dict[str, tp.Dict[str, Seq]]:
-    primer_sequences = SeqIO.parse(primer_file, "fasta")
-    primer_dict = {}
-    for primer in primer_sequences:
-        region = "_".join(primer.name.split("_")[:-1])
-        orientation = primer.name.split("_")[-1]
-        if not region in primer_dict.keys():
-            primer_dict[region] = {}
-        if orientation not in primer_dict[region].keys():
-            primer_dict[region][orientation] = Seq(str(primer.seq))
-    return {
-        region: primers for region, primers in primer_dict.items()
-        if "F" in primers.keys() and "R" in primers.keys()
-    }
-
-
 def fragment_worker(
     fasta_record: SeqRecord,
     primer_dict: tp.Dict[str, Seq],
@@ -427,21 +372,23 @@ def build_fragment_database(
     writer.close()
 
 
-
-
 zipper = lambda x: dict(zip([func.__name__ for func in x], x))
 REDUCERS = zipper([
     reduce_randomly,
     reduce_by_centroid,
 ])
-
+MATCHERS = zipper([
+    align_matcher,
+    exact_matcher,
+    fuzzy_matcher,
+])
 
 def main(
     fasta_file: str,
     primer_file: str,
     outfile: str,
     reducer: str = "reduce_randomly",
-    matcher: tp.Callable = align_matcher,
+    matcher: str = "align_matcher",
     threshold: float = 0.8,
     reverse_primer3: bool = True,
     n_jobs: int = 1,
@@ -456,19 +403,13 @@ def main(
         fasta_file=reduced_fasta,
         primer_file=primer_file,
         outfile=outfile,
-        matcher=matcher,
+        matcher=MATCHERS[matcher],
         threshold=threshold,
         reverse_primer3=reverse_primer3,
         n_jobs=n_jobs,
     )
 
 
-
-MATCHERS = zipper([
-    align_matcher,
-    exact_matcher,
-    fuzzy_matcher,
-])
 LOG_LEVELS = dict(sorted([
     (name, lvl) for name, lvl in logging._nameToLevel.items()
 ], key=lambda x: x[1]))
@@ -560,7 +501,7 @@ def cli(*args):
         fasta_file=os.path.abspath(cliargs.fasta_file),
         primer_file=os.path.abspath(cliargs.primer_file),
         outfile=os.path.abspath(cliargs.outfile),
-        matcher=MATCHERS[cliargs.matcher],
+        matcher=cliargs.matcher,
         threshold=cliargs.threshold,
         reverse_primer3=cliargs.reverse_primer3,
         n_jobs=cliargs.n_jobs,
---
+++ frag16S/utils.py
@@ -0,0 +1,71 @@
+from Bio.SeqRecord import SeqRecord
+from Bio.Seq import Seq
+from Bio import SeqIO
+import typing as tp
+import hashlib
+import pickle
+import os, re
+import json
+import os
+
+
+def hash_sequence(sequence, size=10):
+    h = hashlib.blake2b(digest_size=size)
+    h.update(pickle.dumps(sequence))
+    return h.hexdigest()
+
+
+def extract_metadata(sequence: SeqRecord) -> tp.Dict[str, str]:
+    metadata = dict(
+        seqid=sequence.id,
+        seqname=sequence.name,
+        seqhash=hash_sequence(sequence.seq),
+        description=sequence.description,
+    )
+    metadata["uname"] = "sequence_{id}_{name}_{hash}".format(
+        id=metadata["seqid"],
+        name=metadata["seqname"],
+        hash=metadata["seqhash"],
+    )
+    search = re.search(
+        r"(\d+) ([\w.]+) ([\w. ]+) (\w+;.+;) ([\w]+)",
+        metadata.get("description", "")
+    )
+    for i, key in enumerate([
+        "dscid",
+        "strnid",
+        "strnname",
+        "taxon",
+        "otu",
+    ]):
+        metadata[key] = search.group(i + 1) if search else "undefined"
+    return metadata
+
+
+def convert_ambiguous_to_regex(primer: str):
+    REGEXER = json.load(open(
+        os.path.abspath(os.path.join(os.path.dirname(__file__), "data/pattern.json"))
+        , "r"))
+    regex = ""
+    for i in range(len(primer)):
+        regex += REGEXER[primer[i]]
+    return regex
+
+
+def build_primer_dict(
+    primer_file: str,
+) -> tp.Dict[str, tp.Dict[str, Seq]]:
+    primer_sequences = SeqIO.parse(primer_file, "fasta")
+    primer_dict = {}
+    for primer in primer_sequences:
+        region = "_".join(primer.name.split("_")[:-1])
+        orientation = primer.name.split("_")[-1]
+        if not region in primer_dict.keys():
+            primer_dict[region] = {}
+        if orientation not in primer_dict[region].keys():
+            primer_dict[region][orientation] = Seq(str(primer.seq))
+    return {
+        region: primers for region, primers in primer_dict.items()
+        if "F" in primers.keys() and "R" in primers.keys()
+    }
+
