373e1b3a94cf9eaaecc1a842b6ec07610c6c7f2a
Pedro Lalanda Delgado <plalandelg@gmail.com>
2025-07-02 13:20:14 +0000

    reorganizing

--- frag16S/generate_database.py
+++ frag16S/generate_database.py
@@ -16,63 +16,11 @@ import traceback
 import tempfile
 import builtins
 import hashlib
-import logging
 import inspect
 import pickle
 import shutil
 import json
 
-
-def track_open(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        original_open = builtins.open
-        def open_logger(file, *a, **k):
-            opened = os.path.abspath(file)
-            logging.debug(f"Opened file {opened}")
-            return original_open(file, *a, **k)
-        builtins.open = open_logger
-        try:
-            result = func(*args, **kwargs)
-        finally:
-            builtins.open = original_open
-        return result
-    return wrapper
-    
-
-def log_step(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        runner = track_open(func)
-        logging.debug(f"Running step {func.__name__}")
-        prototype = inspect.signature(func)
-        logging.debug(f"Prototype: {func.__name__}{prototype}")
-        bound = prototype.bind(*args, **kwargs)
-        bound.apply_defaults()
-        params = [
-            (k, v) for k, v in bound.arguments.items()
-            if k not in ["args", "kwargs"]
-        ]
-        ag = bound.arguments.get("args")
-        kw = bound.arguments.get("kwargs")
-        if ag:
-            params += [
-                (f"args_{i}", ag[i]) for i in range(len(ag))
-            ]
-        if kw:
-            params += [(k, v) for k, v in kw.items()]
-        table = tabulate(
-            params, headers=["Parameter", "Value"], tablefmt="pretty"
-        )
-        logging.debug(f"Arguments:\n{table}")
-        try:
-            return runner(*args, **kwargs)
-        except Exception as e:
-            logging.error(f"Error while running step {func.__name__}")
-            logging.error(f"Exception: {e}")
-            logging.error(f"Traceback:\n{traceback.format_exc()}")
-            raise e
-    return wrapper
         
 
 def hash_sequence(sequence, size=10):
@@ -479,30 +427,7 @@ def build_fragment_database(
     writer.close()
 
 
-def setup_logging(
-    log_level: int = 20,
-    log_file: str = None,
-    log_format: str = '[%(asctime)s | %(levelname)s] : %(message)s'
-):
-    logger = logging.getLogger()
-    logger.setLevel(log_level)
-    formatter = logging.Formatter(log_format)
-    if not any(
-        isinstance(handler, logging.StreamHandler)
-        for handler in logger.handlers
-    ):
-        stream = logging.StreamHandler()
-        stream.setLevel(log_level)
-        stream.setFormatter(formatter)
-        logger.addHandler(stream)
-    if not any(
-        isinstance(handler, logging.StreamHandler)
-        for handler in logger.handlers
-    ) and not isinstance(log_file, type(None)):
-        fileh = logging.FileHandler(filename=log_file)
-        fileh.setLevel(log_level)
-        fileh.setFormatter(formatter)
-        logger.addHandler(fileh)
+
 
 zipper = lambda x: dict(zip([func.__name__ for func in x], x))
 REDUCERS = zipper([
